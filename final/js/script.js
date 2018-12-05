0a9ec4230c3951cb5387054acd54293a9194f67d
Ryoon320 <43006622+Ryoon320@users.noreply.github.com>
2018-12-05 09:28:01 -0600

    Add files via upload
    final resize

---
+++ final/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+// function multiplyNode(node, count, deep) {
+//     for (var i = 0, copy; i < count - 1; i++) {
+//         copy = node.cloneNode(deep);
+//         node.parentNode.insertBefore(copy, node);
+//     }
+// }
+
+// multiplyNode(document.querySelector('.box'), 3, true)
+
+
+
+
+});
+
